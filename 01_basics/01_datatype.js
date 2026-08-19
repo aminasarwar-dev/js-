cd91c4eddff5ce0547dd5dc1927c760a35e66666
Amina Sarwar <aminasarwar100@gmail.com>
2026-08-19 14:44:35 +0000

    datatypes

---
+++ 01_basics/01_datatype.js
@@ -0,0 +1,18 @@
+"use strict";
+//alert(2+3)
+//datatypes in js
+//number
+//bigint
+//string
+//bolean
+//undefined
+//null
+//symbol
+//object
+let name1="amina"
+let age =23
+let isLoggedIn=true
+console.table([name1,age,isLoggedIn])
+console.table(typeof age)
+console.table(typeof name1)
+console.table(typeof isLoggedIn)
---
+++ 01_basics/01_variable.js
@@ -0,0 +1,11 @@
+const accountId=1441
+let accountEmail="amina@gmail.com"
+var accountPassword="14321"
+accountCity="kharian"
+
+//accountId=2
+accountEmail="amina12@gmail.com"
+accountPassword="1422321"
+accountCity="dinga"
+console.log(accountId)
+console.table([accountEmail,accountCity,accountPassword])
